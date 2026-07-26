356ba3d598239667c9b8787dd7a3b3f7e92cdac2
Anurag Gautam <102229337+gautam-anurag@users.noreply.github.com>
2026-07-26 16:24:45 +0530

    Update

---
+++ meds/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'qubit-meds-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  'manifest.json',
+  'https://gautam-anurag.github.io/qubit/assets/qubit-icon.png'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event (Network falling back to cache strategy)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
